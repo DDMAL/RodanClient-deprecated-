7a3c16f9f9adccec2cdd39242442a1ca007927ae
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-03-12 12:05:12 -0400

    set idAttribute on User model

--- rodan/app/User/User.js
+++ rodan/app/User/User.js
@@ -7,9 +7,9 @@ class User extends Backbone.Model
 {
     constructor(data)
     {
+        this.idAttribute = 'url';
         super(data);
         this.rodanChannel = Radio.channel('rodan');
-
         this.appInstance = this.rodanChannel.request(Events.CurrentApplication);
     }
 }
