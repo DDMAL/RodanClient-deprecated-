36fc22ca0a21b3e054a83f4cea3a495224faf33c
Evan Magoni <esm367@gmail.com>
2015-05-22 10:43:54 -0400

    Fixed: Login form triggers login function
    In order to allow Backbone to get the data from the login form, I had to override the browser's native submit functionality.

--- rodan/app/User/LoginView.js
+++ rodan/app/User/LoginView.js
@@ -6,26 +6,42 @@ import _ from 'underscore'
 import Events from '../Events';
 import Radio from 'backbone.radio';
 
-class LoginView extends Backbone.Marionette.ItemView {
-    constructor(options) {
+class LoginView extends Backbone.Marionette.ItemView
+{
+    constructor(options)
+    {
         super(options);
 
         this.template = '#login-form';
         this.rodanChannel = Radio.channel('rodan');
     }
 
-    events() {
+    events()
+    {
         return {
-            'click #login-btn': 'loginAttempt'
+            'click #login-btn': 'loginAttempt',
+            'keyup input[name="password"]': 'onKeyUp'
         };
     }
 
-    loginAttempt() {
+    loginAttempt()
+    {
         //@TODO form validation!
         var username = this.$('input[name="username"]').val();
         var password = this.$('input[name="password"]').val();
         this.rodanChannel.trigger(Events.AuthenticationAttempt, {'user': username, 'pass': password});
         //appInstance.AuthenticationController.login(username, password);
+    }
+
+    onKeyUp(ev)
+    {
+        var key = ev.keyCode || ev.which;
+
+        if (key == 13 && $('input[name="password"]').val()) {
+            ev.preventDefault();
+            this.loginAttempt();
+            return false;
+        }
     }
 
 }
