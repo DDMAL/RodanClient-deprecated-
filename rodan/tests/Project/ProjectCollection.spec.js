6997f90bbc5e2e9b7c16d0f83f8d8e291282b0bf
Evan Magoni <esm367@gmail.com>
2015-05-25 13:36:17 -0400

    Tests: ProjectCollection added
    This test checks whether ProjectCollection adds the appropriate Project models based from the /projects/ route response. The second argument to it() is a callback that tells Jasmine that it is time to evaluate the expect() calls (necessary because we are testing an asynchronous operation).

--- rodan/tests/Project/ProjectCollection.spec.js
+++ rodan/tests/Project/ProjectCollection.spec.js
@@ -1,10 +1,28 @@
 import sinon from 'sinon';
-import ProjectCollection from '../../app/Project/ProjectCollection';
+import Events from '../../app/Events'
+import Radio from 'backbone.radio'
+import ProjectCollection from '../../app/Project/ProjectCollection'
+import ServerController from '../../app/Shared/ServerController'
 
 describe('Project Collection', function()
 {
     beforeEach(function()
     {
+        this.server = sinon.fakeServer.create();
+        this.configuration = {
+            rodanServer: 'http://example.com/',
+            authenticationType: 'token',               // 'session' or 'token'
+            authenticationToken: null
+        };
+
+        this.rootResponse = '{"routes":{"taskqueue-config":"http://example.com/taskqueue/config/","connections":"http://example.com/connections/","resultspackages":"http://example.com/resultspackages/","workflowjobs":"http://example.com/workflowjobs/","token-auth":"http://example.com/auth/token/","session-close":"http://example.com/auth/logout/","taskqueue-status":"http://example.com/taskqueue/status/","session-status":"http://example.com/auth/status/","workflowruns":"http://example.com/workflowruns/","resources":"http://example.com/resources/","inputs":"http://example.com/inputs/","jobs":"http://example.com/jobs/","users":"http://example.com/users/","runjobs":"http://example.com/runjobs/","outputs":"http://example.com/outputs/","inputporttypes":"http://example.com/inputporttypes/","session-auth":"http://example.com/auth/session/","inputports":"http://example.com/inputports/","workflows":"http://example.com/workflows/","taskqueue-scheduled":"http://example.com/taskqueue/scheduled/","taskqueue-active":"http://example.com/taskqueue/active/","projects":"http://example.com/projects/","outputporttypes":"http://example.com/outputporttypes/","outputports":"http://example.com/outputports/","resourcetypes":"http://example.com/resourcetypes/"},"configuration":{"page_length":20}}';
+        this.server.respondWith("GET", "http://example.com/", [200, { "Content-Type": "application/json"}, this.rootResponse]);
+        this.serverController = new ServerController(this.configuration);
+        this.server.respond();
+
+        this.rodanChannel = Radio.channel('rodan');
+        this.rodanChannel.reply(Events.CurrentApplication, this);
+
         this.projectCollection = {
                 count: 2,
                 next: null,
@@ -40,11 +58,28 @@ describe('Project Collection', function()
                     }
                 ]
             };
-    });
-    
-    it('should construct a new project collection from a paged JSON response', function()
-    {
 
     });
     
+    it('should construct a new project collection from a paged JSON response', function(done)
+    {
+        var spy = sinon.spy();
+        var newProjectCollection = new ProjectCollection();
+
+        this.server.respondWith("GET", "http://example.com/projects/", [200, { "Content-Type": "application/json"}, JSON.stringify(this.projectCollection)]);
+        newProjectCollection.fetch();
+
+        newProjectCollection.on('add', function(changed) {
+            spy(changed.attributes.name);
+            expect(spy.calledWithExactly('test')).toBe(true);
+            done();
+        });
+
+        this.server.respond();
+    });
+
+    afterEach(function()
+    {
+        this.server.restore();
+    });
 });
