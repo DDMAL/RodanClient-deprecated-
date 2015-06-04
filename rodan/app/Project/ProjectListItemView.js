7f3f267b6bc4b47b97a7c4153e88f2f74c3cc870
Evan Magoni <esm367@gmail.com>
2015-06-04 15:36:33 -0400

    New: project list clickable
    Clicking on a row in the project list (corresponding to a single ProjectListItemView) will eventually take us to a ProjectView (project detail/edit view).

--- rodan/app/Project/ProjectListItemView.js
+++ rodan/app/Project/ProjectListItemView.js
@@ -4,11 +4,24 @@ class ProjectListItemView extends Marionette.ItemView
 {
     get template()
     {
-        return '#project-view';
+        return '#project-list-item-view';
     }
     get tagName()
     {
         return 'tr';
+    }
+    events()
+    {
+        return {
+            'click': 'showProject'
+        }
+    }
+
+    showProject(event)
+    {
+        var projectID = this.model.get('uuid'); // model.id isn't working for some reason
+        var url = this.model.get('url');
+        this.trigger('ProjectSelected', {projectID: projectID, projectURL: url});
     }
 }
 
