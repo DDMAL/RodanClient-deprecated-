e365641b9314b9ae41185e4a6368fbf08bc678fb
Evan Magoni <esm367@gmail.com>
2015-06-05 17:14:25 -0400

    Fixed: remove unnecessary imports
    Our models/collections don't need to know about Radio or the application instance—they are simple containers.

--- rodan/app/Workflow/WorkflowCollection.js
+++ rodan/app/Workflow/WorkflowCollection.js
@@ -1,15 +1,11 @@
 import Backbone from 'backbone';
-import Radio from 'backbone.radio';
 import WorkflowListItem from './WorkflowListItem';
-import Events from '../Events';
 
 class WorkflowCollection extends Backbone.Collection
 {
     constructor(options)
     {
-        this.rodanChannel = Radio.channel('rodan');
         this.model = WorkflowListItem;
-        var appInstance = this.rodanChannel.request(Events.CurrentApplication);
 
         super(options);
     }
